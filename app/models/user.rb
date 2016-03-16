37116d97981f64e8cf582680f60fe3109d638b9f
Albert Pai <albert@pai.email>
2016-03-15 20:48:46 -0700

    User has_many articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
 
   validates :username, uniqueness: true, presence: true, allow_blank: false
 
+  has_many :articles
+
   def generate_jwt
     JWT.encode({ id: self.id,
                  username: self.username,
